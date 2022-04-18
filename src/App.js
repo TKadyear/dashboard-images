c8bcb1bc962fb19a5f9aef2820a372b36cfa8fc2
TKadyear <tamarakadyear@gmail.com>
2022-04-18 16:40:51 +0200

    Added documentation Unsplash

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 import Button from '@mui/material/Button';
+import { createApi } from 'unsplash-js';
+const api = createApi({
+  // Don't forget to set your access token here!
+  // See https://unsplash.com/developers
+  accessKey: process.env.ACCESS_KEY_UNSPLASH
+});
 function App() {
+  const [image, setImage] = useState();
+  useEffect(() => {
+
+  }, [])
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <Button variant="contained">Contained</Button>
+
+        <Button variant="contained">Hi {process.env.NOMBRE}</Button>
       </header>
     </div>
   );
