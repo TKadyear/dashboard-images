a5de650b1c5f790091f869379e8e8327c32b441d
TKadyear <tamarakadyear@gmail.com>
2022-04-21 11:11:05 +0200

    Adding Component for seeing the list of data

---
+++ src/components/image-list.js
@@ -0,0 +1,25 @@
+import ImageList from '@mui/material/ImageList';
+import ImageListItem from '@mui/material/ImageListItem';
+import ImageListItemBar from '@mui/material/ImageListItemBar';
+
+export default function TitlebarBelowImageList(props) {
+  return (
+    <ImageList sx={{ width: 500, height: 450 }}>
+      {props.itemData.map((item) => (
+        <ImageListItem key={item.img}>
+          <img
+            src={`${item.img}?w=248&fit=crop&auto=format`}
+            srcSet={`${item.img}?w=248&fit=crop&auto=format&dpr=2 2x`}
+            alt={item.title}
+            loading="lazy"
+          />
+          <ImageListItemBar
+            title={item.title}
+            subtitle={<span>by: {item.author}</span>}
+            position="below"
+          />
+        </ImageListItem>
+      ))}
+    </ImageList>
+  );
+}
