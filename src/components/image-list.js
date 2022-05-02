2066dba87b718425d0825e648e5c4049fe15da57
TKadyear <tamarakadyear@gmail.com>
2022-05-02 09:49:52 +0200

    WIP - Added Modal for editing

--- src/components/image-list.js
+++ src/components/image-list.js
@@ -1,32 +1,17 @@
 import { ImageList, ImageListItem, ImageListItemBar, Button } from "@mui/material";
-import { useState, useEffect } from "react";
-import { useSelector, useDispatch } from "react-redux";
-import { editDescription, findPhoto } from "../features/my-photos/myPhotosSlice";
-
-// TODO Hacer que sea un componente más grande para reutilizar
-const EditText = (props) => {
-	const photo = useSelector(findPhoto(props.id));
-	const dispatch = useDispatch();
-	const handleEdit = () => {
-		props.finishEdit();
-		const payload = { description: editTerm, id: props.id };
-		dispatch(editDescription(payload));
-	};
-	const [editTerm, setEditTerm] = useState("");
-	const handleChange = (e) => setEditTerm(e.target.value);
-	useEffect(() => setEditTerm(photo.description || ""), []);
-
-	return (
-		<>
-			<input type="text" value={editTerm} onChange={handleChange} />
-			<button onClick={handleEdit}>Guardar</button>
-		</>
-	);
-};
-// TODO una comprobación de cual es el id esta activo para que solo se active para editar ahi.
-// TODO el dispatch al
+import { useState } from "react";
+import { EditText } from "./ModalEditDescription";
 export const GalleryImages = (props) => {
 	const [isEditing, setIsEditing] = useState("");
+	const [open, setOpen] = useState("");
+	const handleEdit = (id) => {
+		setIsEditing(id);
+		setOpen(true);
+	};
+	const handleClose = () => {
+		setIsEditing("");
+		setOpen(false);
+	};
 	return (
 		<ImageList cols={4} gap={1} sx={{
 			width: "75%",
@@ -50,7 +35,10 @@ export const GalleryImages = (props) => {
 							<p>{item.description}</p>
 							<a href={item.download} download>Download</a>
 							<button onClick={() => props.onClickRemove(item)}>Remove</button>
-							{isEditing === item.id ? <EditText valueToChange={item.description} onSubmit={() => props.submitEdit()} finishEdit={() => setIsEditing("")} id={item.id} /> : <button onClick={() => setIsEditing(item.id)}>Edit</button>}
+							{isEditing === item.id
+								? <EditText open={open} onClose={handleClose} id={item.id} />
+								: <button onClick={() => handleEdit(item.id)}>Edit</button>
+							}
 						</div>
 					)}
 				</ImageListItem>
