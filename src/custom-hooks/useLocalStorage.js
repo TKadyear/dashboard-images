9b3f30da717e5a7e5cb5e90283cb0081e402c990
TKadyear <tamarakadyear@gmail.com>
2022-05-02 13:34:52 +0200

    Removed useless files

--- src/custom-hooks/useLocalStorage.js
+++
@@ -1,9 +0,0 @@
-import { useState, useEffect } from "react";
-
-const getLocalStorage = (idItem, defaultValue) => JSON.parse(localStorage.getItem(idItem)) || defaultValue;
-export const useLocalStorage = ([idItem, defaultValue]) => {
-  const [item, setItem] = useState(() => {
-    const saveInLocalStorage = getLocalStorage(idItem, defaultValue);
-  });
-
-};
