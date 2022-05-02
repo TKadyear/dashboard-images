4ef5f08fc1c1cc81f340a694d4f95d50dfb8f1dc
TKadyear <tamarakadyear@gmail.com>
2022-05-02 10:58:03 +0200

    Added the layout for the filter Bar

--- src/components/FilterBar.js
+++ src/components/FilterBar.js
@@ -1,6 +1,14 @@
 import { IconButton, Select, InputLabel, MenuItem, FormControl } from "@mui/material";
 import ArrowDownwardIcon from "@mui/icons-material/ArrowDownward";
 import ArrowUpwardIcon from "@mui/icons-material/ArrowUpward";
+import styled from "@emotion/styled";
+const Filter = styled.div`/*css */
+  padding: 2rem;
+  display:flex;
+  flex-flow: row wrap;
+  justify-content: center;
+  margin-bottom: 0.5rem ;
+`;
 export const FilterBar = (props) => {
   const menuOptions = () => {
     const allOptions = Object.keys(props.optionsFilter).map(option =>
@@ -9,7 +17,7 @@ export const FilterBar = (props) => {
     return allOptions;
   };
   return (
-    <div>
+    <Filter>
       {props.children}
       {props.sortOptions.isAscending
         ? <IconButton aria-label="ascending" onClick={props.onClick}><ArrowUpwardIcon /> </IconButton>
@@ -18,11 +26,10 @@ export const FilterBar = (props) => {
       <FormControl>
 
         <InputLabel id="sort-options">Sort Options:</InputLabel>
-        <Select autoWidth={true} labelId="sort-options" label="Sort" value={props.sortOptions.optionActive} name="Order" onChange={(e) => props.onChange(e.target.value)}>
+        <Select sx={{ width: "150px" }} labelId="sort-options" label="Sort" value={props.sortOptions.optionActive} name="Order" onChange={(e) => props.onChange(e.target.value)}>
           {menuOptions()}
         </Select>
       </FormControl>
-      <hr />
-    </div >
+    </Filter >
   );
 };
