7ea0c2c4bedeee4ff611ffa2f6ab746de21f0c8f
TKadyear <tamarakadyear@gmail.com>
2022-05-06 11:43:58 +0200

    Added better layout for the home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,27 +1,34 @@
-import { Typography, Box, Button } from "@mui/material";
+import { Typography, Box, Button, Container } from "@mui/material";
 import SearchIcon from "@mui/icons-material/Search";
-import styled from "@emotion/styled";
 import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import { getRandomPhoto } from "../services/unsplash-api";
 
-const Container = styled.div`/*css*/
-	display: flex;
-	flex-direction: row;
-	flex-wrap: wrap;
-	justify-content: space-around;
-	align-items: center;
-	/* text-align: center; */
-	gap: 2rem;
-	padding: 2rem;
-	`;
-const Image = styled.div`/*css*/
-	width: 40%;
-	height: 60vh;
-	background: url(${props => props.bg}) no-repeat center;
-	background-size: cover;
-	border-radius: 25% 25% 0 0;
-`;
+const styleContainer = (theme) => ({
+	display: "flex",
+	flexDirection: "column-reverse",
+	padding: "2rem",
+	gap: "2rem",
+	[theme.breakpoints.up("sm")]: {
+		flexDirection: "row",
+		justifyContent: "space-around",
+		alignItems: "center"
+	},
+});
+const styleImage = (bg) => (theme) => ({
+	width: "80vw",
+	height: "200px",
+	background: `url(${bg}) no-repeat center`,
+	backgroundSize: "cover",
+	borderRadius: "12px",
+	[theme.breakpoints.up("sm")]: {
+		width: "40%",
+		height: "60vh",
+		flexDirection: "row",
+		justifyContent: "space-around",
+		alignItems: "center"
+	},
+});
 
 export const Home = () => {
 	const [background, setBackground] = useState("");
@@ -31,20 +38,19 @@ export const Home = () => {
 	}, []);
 	return (
 		<>
-			<Container>
-				<Box sx={{ width: "45%" }}>
-					<Typography variant="h1" sx={{ fontSize: "4rem" }}>
+			<Container sx={styleContainer}>
+				<Box sx={{ width: { xs: "100%", sm: "65%", md: "45%" } }} >
+					<Typography variant="h1" sx={{ fontSize: { xs: "2.5rem", sm: "3rem", md: "4rem" }, margin: "1rem auto" }}>
 						All the assets you need, in one place
 					</Typography>
-					<Typography variant="h1" sx={{ fontSize: "1.75rem" }}>
+					<Typography variant="h2" sx={{ fontSize: { xs: "1rem", sm: "1.5rem", md: "1.75rem" }, margin: "1rem auto" }}>
 						Find and download the best high-quality photos
 					</Typography>
 					<Button variant="contained" to="/dashboard-images/search" endIcon={<SearchIcon />} component={Link}>
 						Search
 					</Button>
 				</Box>
-
-				<Image bg={background} />
+				<Box sx={styleImage(background)} />
 			</Container>
 		</>
 	);
